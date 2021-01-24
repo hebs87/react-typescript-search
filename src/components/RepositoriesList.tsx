dead77f4108992815be51946300340bc46d02a31
sunny <sunny@jbcole.co.uk>
2021-01-24 00:56:41 +0000

    Added onInputChange and initial onSubmit function to form in RepositoriesList

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,20 @@
-import React, {FC, ReactElement} from "react";
+import React, {FC, ReactElement, useState} from "react";
 
 const RepositoriesList: FC = (): ReactElement => {
+  const [term, setTerm] = useState('');
+
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setTerm(event.target.value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
-      <form>
-        <input type="text"/>
+      <form onSubmit={onSubmit}>
+        <input type="text" onChange={onInputChange} value={term}/>
         <button type="submit">Search</button>
       </form>
     </div>
